1953959962d904493efa7f633ff2e537a8844295
Amadeo G <105983635+Amadeo-G@users.noreply.github.com>
2022-07-05 21:58:30 -0300

    Se sube desde VSC el primer ejercicio.

---
+++ Ejercicio N° 01.py
@@ -0,0 +1,11 @@
+# Escribí un programa que solicite a un usuario que ingrese su nombre.
+# Almacenalo en una variable llamada "nombre".
+# Mostrá en la pantalla el texto “Qué bueno que éstas practicando Python, [nombre]”.
+ 
+# Ejemplo de ejecución:
+
+# Tu nombre: Soledad
+# ¡Qué bueno que éstas practicando Python, Soledad!
+
+nombre=input("Tu nombre: ")
+print("¡Qué bueno que estás practicando Python, " + nombre + "!")
